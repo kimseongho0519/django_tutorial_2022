539752808d1421dddb6ec810fdda4f5bf98bd02e
kimseongho0519 <rlatjdgh1283@naver.com>
2022-03-17 15:53:24 +0900

    form 입력한 데이터 저장하기

--- django_tutorial/dashboard/views.py
+++ django_tutorial/dashboard/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render
+from multiprocessing import reduction
+from django.shortcuts import redirect, render
 from .models import CountryData
+from .forms import CountryDataForm
 # Create your views here.
 # 함수 안에는 request 
 def dashboard(request):
     #각 나라와 인구숫자 가져오기
     data = CountryData.objects.all()
-    #print(data)
+    # add 버튼 클릭 , 값 입력 요청 처리
+    if request.method =='POST':
+    #DB입력
+        form = CountryDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('.')
+    #form 출력 
+    else:
+        #form 객체 생성
+        form = CountryDataForm()
+    
 
     #변수처리, db데이터 가져오기
     #데이터 필터링, form처리
     #aaa="대시보드 만들기"
+    #form = CountryDataForm()
+    context = {'dataset': data, 'form': form }
     return render(request, 
-    'dashboard/dashboard.html',{'dataset': data} )
+    'dashboard/dashboard.html', context)
     # return render(request, '랜더링할 템플릿파일',{템플릿에 전달할 데이터 키: 데이터 변수} )
 
 
